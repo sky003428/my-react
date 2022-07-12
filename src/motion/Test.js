22965c29b1a9294129262e11669036ca39cd28fc
TommyLin <sky003428@gmail.com>
2022-07-13 01:37:56 +0800

    0713

--- src/motion/Test.js
+++ src/motion/Test.js
@@ -1,9 +1,12 @@
 import { motion } from 'framer-motion';
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useRef } from 'react';
+import './test.css';
 
 function Test() {
     const [animateObj, setAnimateObj] = useState({ scale: 1 });
     const [isShow, setIsShow] = useState(false);
+    const constraintsRef = useRef(null);
+    const [customCursor, setCustomCursor] = useState('grab');
 
     const add = useCallback(() => {
         setAnimateObj({ ...animateObj, scale: animateObj.scale + 1 });
@@ -22,8 +25,10 @@ function Test() {
     };
 
     return (
-        <>
+        <div>
+            {/* 每點一次scale+1 */}
             <motion.div
+                className="mr-auto"
                 onClick={add}
                 initial="hidden"
                 style={{
@@ -33,7 +38,9 @@ function Test() {
                 }}
                 animate={animateObj}
             />
+            {/* opacity toggle */}
             <motion.div
+                className="mr-auto"
                 style={{
                     width: '100px',
                     height: '100px',
@@ -47,16 +54,41 @@ function Test() {
                 variants={variants}
             />
             <motion.div
-                drag="x"
-                dragConstraints={{ left: -50, right: 100 }}
-                style={{
-                    width: '100px',
-                    height: '100px',
-                    backgroundColor: '#faa',
-                }}
-                whileTap={{ scale: 0.9 }}
-            />
-        </>
+                drag
+                className="drag-wrap mr-auto d-flex d-justify-contents d-align-items"
+                ref={constraintsRef}
+            >
+                <motion.div
+                    drag
+                    dragConstraints={constraintsRef}
+                    dragMomentum={false}
+                    style={{
+                        // width: '200px',
+                        height: '100%',
+                        backgroundPosition: 'center center',
+                        aspectRatio: 'auto 650 / 433',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                        cursor: `${customCursor}`,
+
+                        backgroundImage:
+                            'url("https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcTGuOfiL1QqUzx0LbryIwaK5OM38J7JxL3RbAIO2SBfg63s8o7DjxiriaSrnc64tAN3QBu8FeuJztoWqdbLChI")',
+                    }}
+                    whileTap={{ opacity: 0.8 }}
+                    transition={{ delay: 1 }}
+                    onMouseDown={() => {
+                        setCustomCursor('grabbing');
+                        console.log('md');
+                    }}
+                    onMouseUp={() => {
+                        setCustomCursor('grab');
+                        console.log('mu');
+                    }}
+                >
+                    {customCursor}
+                </motion.div>
+            </motion.div>
+        </div>
     );
 }
 export default Test;
